6296dfae4f1d572634d3335876b2735b5a277a39
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-10-19 17:43:08 +0700

    menambahkan get array elements

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -2,13 +2,45 @@
 
 namespace QuickSort
 {
-class Program
- {
-    //array of integers to hold values
+    class Program
+    {
+        //array of integers to hold values
         private int[] arr = new int[20];
         private int cmp_count = 0; //number of comparasion
         private int nov_count = 0; //number of movements
-        
+
+        // Number of elements in array
+        private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
+
+            //get array elements
+            for(int i =0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+
+
+        {
+
+        }
+    }
+}
+
+
 
 
 
