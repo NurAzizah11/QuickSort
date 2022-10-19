32bd26071d525b3d265975514641307e2bd7d448
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-10-19 17:23:53 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuickSort
+{
+class Program
+ {
+    //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int nov_count = 0; //number of movements
+        
+
+
+
