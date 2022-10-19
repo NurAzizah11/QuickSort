a6238c815b1471dd806d6c2a5c065d69deeccd89
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-10-19 17:50:42 +0700

    menambahkan method swap dan q_sort

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -29,9 +29,30 @@ namespace QuickSort
             Console.WriteLine("\n==================");
 
             //get array elements
-            for(int i =0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[1] = Int32.Parse(s1);
+            }
+        }
+        //swaps the elements at index x with the elements at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low > high)
+                return;
+
+        }
 
 
         {
